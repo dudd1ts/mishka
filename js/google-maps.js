b5051b02baeeb92addb104554c7f77002f6965e2
Sergey Kondrashev <kserg90@gmail.com>
2017-04-11 13:13:01 +0300

    Добавил изображения для ретины

--- js/google-maps.js
+++ js/google-maps.js
@@ -1,13 +1,19 @@
 function initMap() {
+  var myLatLng = {lat: 59.9387942, lng: 30.3230833};
+
   var map = new google.maps.Map(document.querySelector(".contacts__map"), {
     zoom: 16,
-    center: {lat: 59.9387942, lng: 30.3230833}
+    center: myLatLng
   });
 
-  var image = 'img/icon-map-pin.svg';
+  var image = {
+    url: "img/icon-map-pin.svg",
+    scaledSize: new google.maps.Size(66, 100)
+};
   var beachMarker = new google.maps.Marker({
-    position: {lat: 59.9387942, lng: 30.3230833},
+    position: myLatLng,
     map: map,
+    optimized: false,
     icon: image
   });
 }
